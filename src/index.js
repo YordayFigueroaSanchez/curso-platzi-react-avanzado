78b0f64f6994749f08571138025e8b982db488d6
yfsanchez <yfsanchez@gmail.com>
2022-08-22 08:38:17 -0300

    agergando babel en la configuracion de webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render('Hola Test', document.getElementById('app'))
+ReactDOM.render(<h1>Test Hello...!!!</h1>, 
+    document.getElementById('app'))
